ac5680fe169b7c6f33064ce13ca8816914202ab7
nicauryS <119086333+nicauryS@users.noreply.github.com>
2024-05-15 16:52:08 -0400

    Add files via upload

---
+++ Hola mundo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        
+        string holaMundo = @"
+ _    _       _          __  __            _      _ 
+| |  | |     | |         |  \/  |          | |    (_)
+| |__| | __ _| | _____   | \  / | __ _ _ __| | __  _ 
+|  __  |/ _` | |/ / _ \  | |\/| |/ _` | '__| |/ / | |
+| |  | | (_| |   <  __/  | |  | | (_| | |  |   < _| |
+|_|  |_|\__,_|_|\_\___|  |_|  |_|\__,_|_|  |_|\_(_) |
+                                                  
+";
+
+        
+        Console.WriteLine(holaMundo);
+
+
+        Console.WriteLine("Presiona cualquier tecla para salir...");
+        Console.ReadKey();
+    }
+}
+
